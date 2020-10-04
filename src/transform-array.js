8d76fa7392888369f7caa4cf4e17395a7af6dd3c
onc157 <onc157@yandex.ru>
2020-10-04 22:37:34 +0300

    solved the transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let transformArr = [];
+  if (!Array.isArray(arr)) throw 'Error';
+  for (i = 0; i < arr.length; i = i + 1) {
+    if ((arr[i] !== '--double-next') && (arr[i] !== '--double-prev') && (arr[i] !== '--discard-next') && (arr[i] !== '--discard-prev')) transformArr.push(arr[i]);
+    else if (arr[i] === '--double-next' && arr[i + 1] !== undefined) transformArr.push(arr[i + 1]);
+    else if (arr[i] === '--double-prev' && arr[i - 1] !== undefined && arr[i - 2] !== '--discard-next') transformArr.push(arr[i - 1]);
+    else if (arr[i] === '--discard-next' && arr[i + 1] !== undefined) i = i + 1;
+    else if (arr[i] === '--discard-prev' && arr[i - 1] !== undefined && arr[i - 2] !== '--discard-next') transformArr.pop();
+  }
+  return transformArr;
 };
