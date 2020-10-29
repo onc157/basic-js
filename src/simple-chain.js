64c7ddddf5090c7f5ba89c54f8acb91731966dc9
onc157 <onc157@yandex.ru>
2020-10-29 12:30:32 +0300

    solved the chain maker task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        this.chain.push(`( ${value} )`);
+        return this;
+    },
+    removeLink(position) {
+        if (position > 0 && position <= this.chain.length) {
+            this.chain.splice(position - 1, 1);
+            return this;
+        } else {
+            this.chain = [];
+            throw 'Error!';
+        }
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let finish = this.chain.join('~~');
+        this.chain = [];
+        return finish;
+    }
 };
 
 module.exports = chainMaker;
