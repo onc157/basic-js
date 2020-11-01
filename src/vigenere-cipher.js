11c98893a9a1c015d1aa02511bd18077aabc2f0c
onc157 <onc157@yandex.ru>
2020-11-01 23:46:56 +0300

    feat: solved the vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,96 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(pointer = true) {
+    this.pointer = pointer;
+    this.tabulaRecta = {
+      a: "abcdefghijklmnopqrstuvwxyz",
+      b: "bcdefghijklmnopqrstuvwxyza",
+      c: "cdefghijklmnopqrstuvwxyzab",
+      d: "defghijklmnopqrstuvwxyzabc",
+      e: "efghijklmnopqrstuvwxyzabcd",
+      f: "fghijklmnopqrstuvwxyzabcde",
+      g: "ghijklmnopqrstuvwxyzabcdef",
+      h: "hijklmnopqrstuvwxyzabcdefg",
+      i: "ijklmnopqrstuvwxyzabcdefgh",
+      j: "jklmnopqrstuvwxyzabcdefghi",
+      k: "klmnopqrstuvwxyzabcdefghij",
+      l: "lmnopqrstuvwxyzabcdefghijk",
+      m: "mnopqrstuvwxyzabcdefghijkl",
+      n: "nopqrstuvwxyzabcdefghijklm",
+      o: "opqrstuvwxyzabcdefghijklmn",
+      p: "pqrstuvwxyzabcdefghijklmno",
+      q: "qrstuvwxyzabcdefghijklmnop",
+      r: "rstuvwxyzabcdefghijklmnopq",
+      s: "stuvwxyzabcdefghijklmnopqr",
+      t: "tuvwxyzabcdefghijklmnopqrs",
+      u: "uvwxyzabcdefghijklmnopqrst",
+      v: "vwxyzabcdefghijklmnopqrstu",
+      w: "wxyzabcdefghijklmnopqrstuv",
+      x: "xyzabcdefghijklmnopqrstuvw",
+      y: "yzabcdefghijklmnopqrstuvwx",
+      z: "zabcdefghijklmnopqrstuvwxy"
+    };
+  }
+
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Error! 2 parameters must be specified!');
+    }
+
+    str = str.toLowerCase();
+    key = key.toLowerCase();
+
+    let encryptedStr = '';
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      let columnIndex = this.tabulaRecta.a.indexOf(key[(i - count) % key.length]);
+
+      if (this.tabulaRecta[str[i]]) {
+        encryptedStr += this.tabulaRecta[str[i]][columnIndex];
+      } else {
+        encryptedStr += str[i];
+        count++;
+      }
+    }
+
+    if (!this.pointer) {
+      encryptedStr = encryptedStr.split('').reverse().join('');
+    }
+
+    console.log(encryptedStr.toUpperCase());
+    return encryptedStr.toUpperCase();
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Error! 2 parameters must be specified!');
+    }
+
+    str = str.toLowerCase();
+    key = key.toLowerCase();
+
+    let decryptedStr = '';
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      let row = this.tabulaRecta[key[(i - count) % key.length]];
+
+      if (row.indexOf(str[i]) !== -1) {
+        decryptedStr += this.tabulaRecta.a[row.indexOf(str[i])];
+      } else {
+        decryptedStr += str[i];
+        count++;
+      }
+    }
+
+    if (!this.pointer) {
+      decryptedStr = decryptedStr.split('').reverse().join('');
+    }
+
+    console.log(decryptedStr.toUpperCase());
+    return decryptedStr.toUpperCase();
   }
 }
 
